1e3023695437e083403e30dc23d33385ffa61c73
Lucas Vieira <dti@sabaojua.com.br>
2022-11-14 14:32:45 -0300

    commite ordinário

--- charts/urls.py
+++ charts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import IndexTemplateView
 
 urlpatterns = [
-    path('', IndexTemplateView.as_view(), name="index" ),
+    path('', IndexTemplateView.as_view(), name="index"),
 ]
